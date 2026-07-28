5dbbf2a4688c5fe5558fd05a48de45c5459df68b
kalekrishna2007-hash <kalekrishna2007@gmail.com>
2026-07-28 09:16:15 +0530

    Time: 13 ms (31.91%) | Memory: 7.9 MB (38.71%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int fib(int n) {
-        int a=0;
-        int b=1;
-        int temp;
-        if(n==0){
-            return a;
-        }
-        for(int i=1;i<n;i++){
-            temp = b;
-            b +=a;
-            a=temp;
+        // int a=0;
+        // int b=1;
+        // int temp;
+        // if(n==0){
+        //     return a;
+        // }
+        // for(int i=1;i<n;i++){
+        //     temp = b;
+        //     b +=a;
+        //     a=temp;
 
-        }
+        // }
 
-        return b;
+        // return b;
+
+        if(n<=1){
+            return n;
+        }
+        int last = fib(n-1);
+        int sec_last = fib(n-2);
+
+        return last+sec_last;
         
     }
 };
